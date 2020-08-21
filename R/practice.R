2fb57a4c8f3634827867deb5d21b870720e6c60b
Adam Hayes <alhayes@uw.edu>
2020-08-21 11:15:41 -0700

    useless comment

--- R/practice.R
+++ R/practice.R
@@ -15,6 +15,7 @@ library(here) # Helper functions for relative filespace referencing
 df <- read_csv(here("data/palmerpenguins.csv"))
 
 # THIS IS A COMMENT
+# modified comment - delete?
 
 # Prepare data ----
 # Transform the data for potential future use in this section! Some ideas are provided below as TODOs.
